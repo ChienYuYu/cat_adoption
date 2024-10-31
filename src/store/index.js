83944b8b945cc0016589e60c895f9629d06560b6
ChienYuYu <jianyou224@gmail.com>
2024-10-31 17:45:24 +0800

    Save changes in index.js before merging

--- src/store/index.js
+++ src/store/index.js
@@ -54,6 +54,7 @@ export default createStore({
   },
   actions: {
     getApi(context) {
+      console.log('10311031^^^^^^^^^^^^');
       const apiUrl = 'https://data.moa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&IsTransData=1';
       axios.get(apiUrl)
         .then((res) => {
