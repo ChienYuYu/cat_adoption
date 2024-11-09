55ac59cb4edde06b7251772d180d6d79e655a757
ChienYuYu <jianyou224@gmail.com>
2024-11-10 01:09:10 +0800

    add pwa

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -2,8 +2,10 @@
   <div class="p-4 bg-dark">
     <p class="text-light text-center mb-0">
       <!-- © 2022 by Chen Chien Yu -->
-      © Established in 2022 by Chen Chien Yu
+      <!-- © Established in 2022 by Chen Chien Yu -->
+      © 2024<br>
+      <span style="color: #aaa;">Established in 2022 by Chen Chien Yu</span>
     </p>
-    <p class="text-center mb-0" style="font-size: 12px; color: #aaa;">v4.2.0</p>
+    <p class="text-center mb-0" style="font-size: 12px; color: #aaa;">v4.3.0</p>
   </div>
 </template>
