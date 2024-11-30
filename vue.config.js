971da349f35ad1b2ef8b6662034fe43e886cda17
ChienYuYu <jianyou224@gmail.com>
2024-11-30 18:44:38 +0800

    vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,23 @@
-// firbase 部署==========
-// 執行firebase deploy
-const { defineConfig } = require('@vue/cli-service');
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/'
-    : '/',
-});
-
-// // github 部署===========
-// // 執行 sh deploy.sh
+// // firbase 部署==========
+// // 執行firebase deploy
 // const { defineConfig } = require('@vue/cli-service');
 
 // module.exports = defineConfig({
 //   transpileDependencies: true,
 
 //   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/meowmeow/'
+//     ? '/'
 //     : '/',
 // });
+
+// github 部署===========
+// 執行 sh deploy.sh
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/meowmeow/'
+    : '/',
+});
